597d248ac7c71966b82fe32b75d080c2af0c3bac
Davou Jobbi <davou.jobbi@Davous-Laptop.local>
2024-07-17 14:11:34 +0100

    ds_store add

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import baseSpec.BaseSpec
+import org.jsoup.Jsoup
 import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.mvc.ControllerComponents
@@ -12,6 +13,8 @@ import play.api.test._
  * You can mock out a whole application including requests, plugins etc.
  *
  * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
+ *
+ * Guice - spins up an application then runs test on said application
  */
 class HomeControllerSpec extends BaseSpec with Injecting with GuiceOneAppPerSuite {
 
@@ -25,25 +28,25 @@ class HomeControllerSpec extends BaseSpec with Injecting with GuiceOneAppPerSuit
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Play Scala API Demo")
     }
 
-    "render the index page from the application" in {
+    "render the index page with the expected header" in {
       val controller = inject[HomeController]
       val home = controller.index().apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      Jsoup.parse(contentAsString(home)).getElementById("header").text() shouldBe("Play Scala API Demo")
     }
 
-    "render the index page from the router" in {
+    "render the index with the expected title" in {
       val request = FakeRequest(GET, "/")
       val home = route(app, request).get
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      Jsoup.parse(contentAsString(home)).title() shouldBe("Play Scala API Demo")
     }
   }
 }
