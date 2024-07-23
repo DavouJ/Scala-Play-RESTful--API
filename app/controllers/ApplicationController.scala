6635a2430bd6d96a9b018cdb7faa03b944a4a286
Davou Jobbi <davou.jobbi@Davous-Laptop.local>
2024-07-23 11:06:35 +0100

    completed logic for all crud methods in application Controller

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -2,7 +2,8 @@ package controllers
 
 import com.google.inject.Singleton
 import models.DataModel
-import play.api.libs.json.Json
+import org.mongodb.scala.result
+import play.api.libs.json.{JsError, JsSuccess, JsValue, Json}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Request, Result}
 import repositories.DataRepository
 
@@ -10,32 +11,48 @@ import scala.concurrent.{ExecutionContext, Future}
 import javax.inject.Inject
 
 
-
 @Singleton
-class ApplicationController @Inject()(dataRepository: DataRepository)(val controllerComponents: ControllerComponents)(implicit ec: ExecutionContext) extends BaseController{
+class ApplicationController @Inject()(dataRepository: DataRepository)(val controllerComponents: ControllerComponents)(implicit ec: ExecutionContext) extends BaseController {
 
-//  def index(): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-//    Future.successful(Ok(views.html.index()))
-//  }
+  //  def index(): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
+  //    Future.successful(Ok(views.html.index()))
+  //  }
 
   def index() = Action.async { implicit request =>
-    dataRepository.index().map{
+    dataRepository.index().map {
       case Right(item: Seq[DataModel]) => Ok {Json.toJson(item)}
       case Left(error) => Status(error)(Json.toJson("Unable to find any books"))
     }
-
   }
-  def read(id: String) = Action.async { implicit request => {
+
+  def read(id: String): Action[AnyContent] = Action.async { implicit request => {
     dataRepository.read(id: String).map {
-      case Right(item) => Ok {Json.toJson(item)}
-      case Left(error) => Status(error)(Json.toJson("Unable to find any books"))
+      item => Ok {Json.toJson(item)}
     }
   }
   }
 
-  def create() = TODO
+  def create() = Action.async(parse.json) { implicit request =>
+    request.body.validate[DataModel] match {
+      case JsSuccess(dataModel, _) => dataRepository.create(dataModel).map(_ => Created)
+      case JsError(_) => Future(BadRequest)
+    }
+  }
 
-  def update(id: String) = TODO
+  def update(id: String) = Action.async(parse.json) { implicit request =>
+    request.body.validate[DataModel] match {
+      case JsSuccess(dataModel, _) => dataRepository.update(id, dataModel).map(_ => Accepted {Json.toJson(dataModel)})
+      case JsError(_) => Future(BadRequest)
+    }
+  }
 
-  def delete(id: String) = TODO
+  def delete(id: String): Action[AnyContent] = Action.async { implicit request => {
+    dataRepository.delete(id: String).map{
+      item => Accepted
+    }
+  }
+  }
+
 }
+
+
--- app/repositories/DataRepository.scala
+++ app/repositories/DataRepository.scala
@@ -5,6 +5,8 @@ import org.mongodb.scala.bson.conversions.Bson
 import org.mongodb.scala.model.Filters.empty
 import org.mongodb.scala.model._
 import org.mongodb.scala.result
+import org.mongodb.scala.result.DeleteResult
+import play.api.http.Status.{BAD_REQUEST, NOT_FOUND}
 import uk.gov.hmrc.mongo.MongoComponent
 import uk.gov.hmrc.mongo.play.json.PlayMongoRepository
 
@@ -29,9 +31,10 @@ class DataRepository @Inject()(mongoComponent: MongoComponent)(implicit ec: Exec
   def index(): Future[Either[Int, Seq[DataModel]]] =    //list of all the dataModels in the database
     collection.find().toFuture().map {
       case books: Seq[DataModel] => Right(books)
-      case _ => Left(404)
+      case _ => Left(NOT_FOUND)
     }
-  def create()(book: DataModel): Future[DataModel] =
+
+  def create(book: DataModel): Future[DataModel] =
     collection.insertOne(book).toFuture().map(_ => book)
 
   private def byID(id: String): Bson =
@@ -39,11 +42,11 @@ class DataRepository @Inject()(mongoComponent: MongoComponent)(implicit ec: Exec
       Filters.equal("_id", id)
     )
 
-  def read(id: String): Future[Either[Int, DataModel]] =
+
+  def read(id: String): Future[DataModel] =
     collection.find(byID(id)).headOption flatMap {
       case Some(data) =>
-        Future(Right(data))
-      case _ => Future(Left(404))
+        Future(data)
     }
 
   def update(id: String, book: DataModel): Future[result.UpdateResult] =
@@ -52,9 +55,17 @@ class DataRepository @Inject()(mongoComponent: MongoComponent)(implicit ec: Exec
       replacement = book,
       options = new ReplaceOptions().upsert(true)).toFuture() /// LOOK into
 
-  def delete(id:String): Future[result.DeleteResult] =
+  def delete(id: String): Future[result.DeleteResult] =
     collection.deleteOne(
-      filter = byID(id)).toFuture()
+      filter = byID(id)
+    ).toFuture()
+
+  //  def delete(id: String): Future[Either[Int,result.DeleteResult]] =
+//    collection.deleteOne(filter = byID(id)).headOption flatMap {
+//      case Some(data) => Future(Right(data))
+//      case _ => Future(Left(NOT_FOUND))
+//    }
+//  //.toFuture()
 
   def deleteAll(): Future[Unit] = collection.deleteMany(empty()).toFuture().map(_ => ())
 
